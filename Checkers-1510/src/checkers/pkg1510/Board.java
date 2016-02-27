d5ff8e239aeee9158779506cc418ead9fde5017a
rmartin- <rmartin-@NE103909.eng.utoledo.edu>
2016-02-26 20:46:01 -0500

    Added project files

---
+++ Checkers-1510/src/checkers/pkg1510/Board.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package checkers.pkg1510;
+
+
+/**
+ *
+ * @author rmartin-
+ */
+public class Board {
+    int board[][] = new int[8][8];
+    
+    /**
+     * 
+     * 
+     * @auth Josh
+     * 
+     * @param move 
+     */
+    public void movePiece(int[] move) {
+        // get starting positon
+        // make 
+    }
+    
+    /**
+     * 
+     * 
+     * @auth Roan
+     * 
+     * @param setup 
+     */
+    public void setupBoard(int[][] setup) {
+        
+    }
+    
+    /**
+     * 
+     * @auth Caleb
+     * 
+     * @return 
+     */
+    public int getSquareState() {
+        return -42;
+    }
+    
+    /**
+     * 
+     * @auth Ashley
+     * 
+     */
+    public void kingPiece() {
+        
+    } 
+    
+}
