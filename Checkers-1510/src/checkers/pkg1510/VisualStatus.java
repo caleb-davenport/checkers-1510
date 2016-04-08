56ccf4c7f120e998b1e0904e357d0e0e25750ad6
caleb-davenport <caleb-davenport@users.noreply.github.com>
2016-04-08 00:29:10 -0400

    Added border between status and board

--- Checkers-1510/src/checkers/pkg1510/VisualStatus.java
+++ Checkers-1510/src/checkers/pkg1510/VisualStatus.java
@@ -19,13 +19,16 @@ import javafx.scene.text.*;
  * @author Caleb Davenport
  */
 public class VisualStatus extends GridPane {
+    private int BORDER_WIDTH = 4;
+    private int STATUS_WIDTH = 196; //200 - BORDER_WIDTH
     Label currentPlayer = new Label("RED");
     VisualStatus() {
         configureStatusPanel();
+        setBackground();
         currentPlayer.setFont(Font.font("Calibri", FontWeight.BOLD, 60));
         currentPlayer.setTextFill(Color.web("F00"));
-        super.add(new Rectangle(25, 25, Color.web("00F")), 0, 1);
-        super.add(currentPlayer, 0, 0);
+        super.add(currentPlayer, 1, 0);
+        super.add(new Rectangle(25, 25, Color.web("00F")), 1, 1);
         if (DEBUG) debug();
     }
     public final void updatePlayer() {
@@ -38,21 +41,40 @@ public class VisualStatus extends GridPane {
             currentPlayer.setTextFill(Color.web("000"));
         }
     }
+    private void setBackground() {
+        Color bg = Color.web("EEE");
+        Color border = Color.web("555");
+        for (int i = 0; i < 4; ++i) {
+            Rectangle rect = new Rectangle(STATUS_WIDTH, 100);
+            Rectangle side = new Rectangle(BORDER_WIDTH, 100);
+            rect.setFill(bg);
+            rect.setMouseTransparent(true);
+            side.setFill(border);
+            side.setMouseTransparent(true);
+            super.add(rect, 1, i);
+            super.add(side, 0, i);
+        }
+    }
     private void configureStatusPanel() {
-       for (int i=0; i<4; i++) {
+        for (int i=0; i<4; i++) {
 	  RowConstraints rowConstraints = new RowConstraints();
 	  rowConstraints.setMinHeight(100);
 	  rowConstraints.setPrefHeight(100);
 	  rowConstraints.setMaxHeight(100);
 	  rowConstraints.setValignment(VPos.CENTER);
 	  this.getRowConstraints().add(rowConstraints);
-       }
-          ColumnConstraints colConstraints = new ColumnConstraints();
-	  colConstraints.setMinWidth(200);
-	  colConstraints.setPrefWidth(200);
-	  colConstraints.setMaxWidth(200);
-	  colConstraints.setHalignment(HPos.CENTER);
-	  this.getColumnConstraints().add(colConstraints);
+        }
+        ColumnConstraints border = new ColumnConstraints();
+        border.setMinWidth(BORDER_WIDTH);
+        border.setPrefWidth(BORDER_WIDTH);
+        border.setMaxWidth(BORDER_WIDTH);
+        this.getColumnConstraints().add(border);
+        ColumnConstraints main = new ColumnConstraints();
+        main.setMinWidth(STATUS_WIDTH);
+        main.setPrefWidth(STATUS_WIDTH);
+        main.setMaxWidth(STATUS_WIDTH);
+        main.setHalignment(HPos.CENTER);
+        this.getColumnConstraints().add(main);
     }
     private void debug() {
         this.setGridLinesVisible(true);
